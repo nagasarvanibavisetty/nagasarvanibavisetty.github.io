15134e4c8d87bf064a9aa21c3abe349113fe3451
Sara <148773275+nagasarvanibavisetty@users.noreply.github.com>
2024-05-20 14:26:58 +0100

    debug

--- style/script.js
+++ style/script.js
@@ -139,6 +139,8 @@ for (let i = 0; i < formInputs.length; i++) {
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
+console.log('navigationLinks: ',navigationLinks)
+console.log("pages: ",pages)
 
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
