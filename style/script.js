7e24ca16f83711c9580fe0077f0817f0ad9477ea
Sara <148773275+nagasarvanibavisetty@users.noreply.github.com>
2024-05-20 14:35:14 +0100

    fix

--- style/script.js
+++ style/script.js
@@ -19,7 +19,7 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
-const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+//const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
 // modal variable
@@ -50,7 +50,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+//modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
 
