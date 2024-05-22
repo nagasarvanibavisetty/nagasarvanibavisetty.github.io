eef1d7a7f8de82e69359f3779444be1e8223f366
SarvaniBavisetty <113676426+SarvaniBavisetty@users.noreply.github.com>
2024-05-22 14:06:15 +0100

    final_hurrah
    i did it!

--- style/script.js
+++ style/script.js
@@ -51,7 +51,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
 // add click event to modal close button
 //modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+//overlay.addEventListener("click", testimonialsModalFunc);
 
 
 
